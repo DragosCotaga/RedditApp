e13e8da3ca520a6764e6fa4bd8b159dae46a11b9
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-10 18:05:53 +0200

    Rose are red, violets are blue, But Troels' jokes, they're something new...
    * Data Classes (Json handlers, file storage and repo)

--- RedditApp/Data/FileStorage/JsonFileStorage.cs
+++ RedditApp/Data/FileStorage/JsonFileStorage.cs
@@ -1,6 +1,32 @@
-﻿namespace RedditApp.Data.FileStorage
+﻿using System.Text.Json;
+
+namespace RedditApp.Data.FileStorage
 {
-    public class JsonFileStorage
+    public class JsonFileStorage<T>
     {
+        private readonly string _filePath;
+
+        public JsonFileStorage(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<T> ReadFromFile()
+        {
+            if (!File.Exists(_filePath))
+            {
+                File.Create(_filePath).Close();
+                return new List<T>();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+
+        public void WriteToFile(List<T> data)
+        {
+            var json = JsonSerializer.Serialize(data);
+            File.WriteAllText(_filePath, json);
+        }
     }
 }
--- RedditApp/Data/Repositories/FileRepository.cs
+++ RedditApp/Data/Repositories/FileRepository.cs
@@ -1,43 +1,44 @@
-﻿using System.Text.Json;
+﻿using RedditApp.Data.FileStorage;
+using System.Text.Json;
 
 namespace RedditApp.Data.Repositories
 {
     public class FileRepository<T> : IRepository<T> where T : class
     {
-        private readonly string _filePath;
+        private readonly JsonFileStorage<T> _fileStorage;
 
         public FileRepository(string filePath)
         {
-            _filePath = filePath;
+            _fileStorage = new JsonFileStorage<T>(filePath);
         }
 
         public List<T> GetAll()
         {
-            return ReadFromFile();
+            return _fileStorage.ReadFromFile();
         }
 
         public T GetById(int id)
         {
-            return ReadFromFile().FirstOrDefault(x => GetId(x) == id);
+            return GetAll().FirstOrDefault(x => GetId(x) == id);
         }
 
         public T Create(T entity)
         {
-            var data = ReadFromFile();
+            var data = GetAll();
             data.Add(entity);
-            WriteToFile(data);
+            _fileStorage.WriteToFile(data);
             return entity;
         }
 
         public T Update(T entity)
         {
-            var data = ReadFromFile();
+            var data = GetAll();
             var index = data.FindIndex(x => GetId(x) == GetId(entity));
 
             if (index != -1)
             {
                 data[index] = entity;
-                WriteToFile(data);
+                _fileStorage.WriteToFile(data);
             }
 
             return entity;
@@ -45,32 +46,14 @@ namespace RedditApp.Data.Repositories
 
         public void Delete(int id)
         {
-            var data = ReadFromFile();
+            var data = GetAll();
             var entity = data.FirstOrDefault(x => GetId(x) == id);
 
             if (entity != null)
             {
                 data.Remove(entity);
-                WriteToFile(data);
+                _fileStorage.WriteToFile(data);
             }
-        }
-
-        private List<T> ReadFromFile()
-        {
-            if (!File.Exists(_filePath))
-            {
-                File.Create(_filePath).Close();
-                return new List<T>();
-            }
-
-            var json = File.ReadAllText(_filePath);
-            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
-        }
-
-        private void WriteToFile(List<T> data)
-        {
-            var json = JsonSerializer.Serialize(data);
-            File.WriteAllText(_filePath, json);
         }
 
         private int GetId(T entity)
